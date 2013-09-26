4f3ab0ffe6e6f14f7566bb6ec1ff9f5351e8852b
Yothin Muangsommuk <yothin@kmi.tl>
2013-09-27 01:41:32 +0700

    end of Day 2 : Database selecting Method

---
+++ application/controllers/site.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: yothi_000
+ * Date: 9/26/13
+ * Time: 11:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class site extends CI_Controller {
+
+    function index(){
+        $this->load->model('data_model');
+        $data['rows'] = $this->data_model->getAll();
+        $this->load->view('home', $data);
+
+
+        //$this->load->model('site_model');
+        //$data['records'] = $this->site_model->getAll();
+        //$this->load->view('home', $data);
+    }
+
+    function about(){
+        $this->load->view('about');
+    }
+
+}
