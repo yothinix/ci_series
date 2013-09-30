e05facb54c94ee05adb59f1e581cd71631e91aef
dummyCSAG <alexsilizer@gmail.com>
2013-09-30 18:00:34 +0700

    Delete PhpStorm Header and Test Multi-User Development

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: yothi_000
- * Date: 9/26/13
- * Time: 11:05 PM
- * To change this template use File | Settings | File Templates.
- */
 
 class site extends CI_Controller {
-
     function index(){
         $this->load->model('data_model');
         $data['rows'] = $this->data_model->getAll();
@@ -18,7 +10,6 @@ class site extends CI_Controller {
         //$data['records'] = $this->site_model->getAll();
         //$this->load->view('home', $data);
     }
-
     function about(){
         $this->load->view('about');
     }
