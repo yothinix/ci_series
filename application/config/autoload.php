198e8ac9c32d04029e5f97d00b701c7a66db7988
Yothin Muangsommuk <yothin@kmi.tl>
2013-11-01 00:33:36 +0700

    Day6 - Login

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database');
+$autoload['libraries'] = array('database', 'session');
 
 
 /*
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('site_model');
+$autoload['model'] = array();
 
 
 /* End of file autoload.php */
---
+++ application/views/signup_form.php
@@ -0,0 +1,28 @@
+<h1>Create an Account</h1>
+
+<fieldset>
+    <legend>Personal Information</legend>
+
+    <?php
+
+    echo form_open('login/create_member');
+    echo form_input('first_name', set_value('firstname', 'First Name'));
+    echo form_input('last_name', set_value('lastname', 'Last Name'));
+    echo form_input('email_address', set_value('email_address', 'Email Address'));
+    ?>
+</fieldset>
+
+<fieldset>
+    <legend>Login Info</legend>
+
+    <?php
+    echo form_input('username', set_value('username', 'Username'));
+    echo form_input('password', set_value('password', 'Password'));
+    echo form_input('password2', set_value('password2', 'Password Confirm'));
+
+    echo form_submit('submit', 'Create Account');
+    ?>
+
+    <?php echo validation_errors('<p class="error">'); ?>
+
+</fieldset>
